2aaec47436791612de988137efaeaa4354f5825b
KushagraYadav28 <kushagrayadav3008@gmail.com>
2026-06-14 13:29:34 +0530

    change gmail smtp port

--- backend/src/services/sendRegistrationEmail.js
+++ backend/src/services/sendRegistrationEmail.js
@@ -1,11 +1,23 @@
 const nodemailer = require('nodemailer');
 
+
+
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+});
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("SMTP Error:", error);
+  } else {
+    console.log("SMTP Ready");
+  }
 });
 
 const sendEmail = async (to, subject, text) => {
